9dea365b5265f436da9b4548e819e70935224f64
umar-khan <76748183+INVADE69200@users.noreply.github.com>
2021-01-01 13:28:09 +0500

    Add files via upload

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const Card=(props)=>{
+  return(
+    <div className='bg-light-green dib br3 pa3 ma2 grow'>
+      <img alt='robots' src={`https://robohash.org/${props.id}?set=set4`} />
+      <div>
+        <h2>{props.myName}</h2>
+        <p>{props.email}</p>
+      </div>
+    </div>
+  )
+}
+export default Card;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './Card';
+import './App.css';
+import 'tachyons';
+import {Robot} from './Robot'
+
+ReactDOM.render(<div>
+  <Card id={Robot[0].id} myName={Robot[0].myName} email={Robot[0].email}/>
+  <Card id={Robot[1].id} myName={Robot[1].myName} email={Robot[1].email}/>
+  <Card id={Robot[2].id} myName={Robot[2].myName} email={Robot[2].email}/>
+  <Card id={Robot[3].id} myName={Robot[3].myName} email={Robot[3].email}/>
+  <Card id={Robot[4].id} myName={Robot[4].myName} email={Robot[4].email}/>
+  <Card id={Robot[5].id} myName={Robot[5].myName} email={Robot[5].email}/>
+</div>,document.getElementById('root')
+);
+
+
+
