7c5de4f7eea8bc739c863052e49c2de71560d55c
umar-khan <76748183+INVADE69200@users.noreply.github.com>
2021-01-07 18:03:59 +0500

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,21 +1,6 @@
 <<<<<<< HEAD
 import React from 'react';
 import CardList from './CardList'
-
-const Card=(props)=>{
-  return(
-    <div className='bg-light-green dib br3 pa3 ma2 grow'>
-      <img alt='robots' src={`https://robohash.org/${props.id}?set=set4`} />
-      <div>
-        {/* <h1>US As Kittens</h1> */}
-
-        <h2>{props.myName}</h2>
-        <p>{props.email}</p>
-      </div>
-    </div>
-  )
-}
-=======
 import React from 'react';
 const Card=(props)=>{
   return(
@@ -28,5 +13,4 @@ const Card=(props)=>{
     </div>
   )
 }
->>>>>>> 9dea365b5265f436da9b4548e819e70935224f64
 export default Card;
